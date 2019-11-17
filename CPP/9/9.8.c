cbc7311310cd06f92ab742a31e72814238b1a657
lhb <853930285@github.com>
2019-11-17 17:07:36 +0800

    19.11.17

---
+++ CPP/9/9.8.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+//转换2进制测试
+void to_binary (unsigned long n)
+{
+	int r;
+	r=n%2;
+	if(n>=2)
+	{
+		to_binary(n/2);
+	}
+	putchar('0'+r);
+	return ;
+}
+
+
+int main()
+{
+	unsigned long num;
+	printf("输入一个值 将会计算其二进制书：");
+	while((scanf("%lu",&num))==1)
+	{
+	to_binary(num);
+	printf("\n");
+	}
+	return 0;
+}
